429ea834ba1f5496c8988d39a5bd81572d975ac7
byt3sage <jtooleuk@gmail.com>
2025-04-03 14:36:05 +0100

    ACD-18

--- src/views/events/Duplicate.vue
+++ src/views/events/Duplicate.vue
@@ -11,88 +11,103 @@
         <gov-grid-row>
           <gov-grid-column width="full">
             <gov-heading size="xl">Events</gov-heading>
-            <gov-heading size="m">Duplicate event</gov-heading>
-            <gov-body
-              >The events will appear on their own page will be discoverable and
-              filterable by visitors based on the information you
-              provide</gov-body
-            >
-            <gov-error-summary
-              v-if="form.$errors.any()"
-              title="Check for errors"
-            >
-              <gov-list>
-                <li
-                  v-for="(error, field) in form.$errors.all()"
-                  :key="field"
-                  v-text="error[0]"
-                />
-              </gov-list>
-            </gov-error-summary>
-            <gov-tabs @tab-changed="onTabChange" :tabs="allowedTabs" no-router>
-              <details-tab
-                v-show="isTabActive('details')"
-                :errors="form.$errors"
-                :id="event.id"
-                @update:title="onUpdateTitle"
-                :title.sync="form.title"
-                :slug.sync="form.slug"
-                :start_date.sync="form.start_date"
-                :end_date.sync="form.end_date"
-                :start_time.sync="form.start_time"
-                :end_time.sync="form.end_time"
-                :intro.sync="form.intro"
-                :description.sync="form.description"
-                :is_free.sync="form.is_free"
-                :fees_text.sync="form.fees_text"
-                :fees_url.sync="form.fees_url"
-                :organiser_name.sync="form.organiser_name"
-                :organiser_phone.sync="form.organiser_phone"
-                :organiser_email.sync="form.organiser_email"
-                :organiser_url.sync="form.organiser_url"
-                :booking_title.sync="form.booking_title"
-                :booking_summary.sync="form.booking_summary"
-                :booking_url.sync="form.booking_url"
-                :booking_cta.sync="form.booking_cta"
-                :homepage.sync="form.homepage"
-                :organisations="organisations"
-                :organisation_id.sync="form.organisation_id"
-                :is_virtual.sync="form.is_virtual"
-                :location_id.sync="form.location_id"
-                :image_file_id.sync="form.image_file_id"
-                @clear="form.$errors.clear($event)"
-                @image-changed="imageChanged = $event"
-                @alt-text-changed="altTextChanged = true"
-              />
-              <taxonomies-tab
-                v-if="isTabActive('taxonomies')"
-                @clear="
-                  form.$errors.clear($event);
-                  errors = {};
-                "
-                :errors="form.$errors"
-                :is-global-admin="auth.isGlobalAdmin"
-                :type="form.type"
-                :category_taxonomies.sync="form.category_taxonomies"
+            <template v-if="updateRequestCreated">
+              <gov-heading size="m" tag="h3">Duplicate event request</gov-heading>
+              <gov-body>{{ updateRequestMessage }}</gov-body>
+              <gov-back-link :to="{ name: 'events-index' }"
+                >Back to events</gov-back-link
               >
-              </taxonomies-tab>
-            </gov-tabs>
-            <gov-warning-text>
-              Please be aware, by submitting these changes, any pending updates
-              may be overwritten.
-            </gov-warning-text>
+            </template>
+            
+            
+            <template v-else>
+              <gov-heading size="m">Duplicate event</gov-heading>
+              <gov-body
+                >The events will appear on their own page will be discoverable
+                and filterable by visitors based on the information you
+                provide</gov-body
+              >
+              <gov-error-summary
+                v-if="form.$errors.any()"
+                title="Check for errors"
+              >
+                <gov-list>
+                  <li
+                    v-for="(error, field) in form.$errors.all()"
+                    :key="field"
+                    v-text="error[0]"
+                  />
+                </gov-list>
+              </gov-error-summary>
+              <gov-tabs
+                @tab-changed="onTabChange"
+                :tabs="allowedTabs"
+                no-router
+              >
+                <details-tab
+                  v-show="isTabActive('details')"
+                  :errors="form.$errors"
+                  :id="event.id"
+                  @update:title="onUpdateTitle"
+                  :title.sync="form.title"
+                  :slug.sync="form.slug"
+                  :start_date.sync="form.start_date"
+                  :end_date.sync="form.end_date"
+                  :start_time.sync="form.start_time"
+                  :end_time.sync="form.end_time"
+                  :intro.sync="form.intro"
+                  :description.sync="form.description"
+                  :is_free.sync="form.is_free"
+                  :fees_text.sync="form.fees_text"
+                  :fees_url.sync="form.fees_url"
+                  :organiser_name.sync="form.organiser_name"
+                  :organiser_phone.sync="form.organiser_phone"
+                  :organiser_email.sync="form.organiser_email"
+                  :organiser_url.sync="form.organiser_url"
+                  :booking_title.sync="form.booking_title"
+                  :booking_summary.sync="form.booking_summary"
+                  :booking_url.sync="form.booking_url"
+                  :booking_cta.sync="form.booking_cta"
+                  :homepage.sync="form.homepage"
+                  :organisations="organisations"
+                  :organisation_id.sync="form.organisation_id"
+                  :is_virtual.sync="form.is_virtual"
+                  :location_id.sync="form.location_id"
+                  :image_file_id.sync="form.image_file_id"
+                  @clear="form.$errors.clear($event)"
+                  @image-changed="imageChanged = $event"
+                  @alt-text-changed="altTextChanged = true"
+                />
+                <taxonomies-tab
+                  v-if="isTabActive('taxonomies')"
+                  @clear="
+                    form.$errors.clear($event);
+                    errors = {};
+                  "
+                  :errors="form.$errors"
+                  :is-global-admin="auth.isGlobalAdmin"
+                  :type="form.type"
+                  :category_taxonomies.sync="form.category_taxonomies"
+                >
+                </taxonomies-tab>
+              </gov-tabs>
+              <gov-warning-text>
+                Please be aware, by submitting these changes, any pending
+                updates may be overwritten.
+              </gov-warning-text>
 
-            <gov-button v-if="form.$submitting" disabled type="submit"
-              >Requesting...</gov-button
-            >
-            <gov-button
-              v-else
-              @click="onSubmit"
-              :disabled="loading || !event"
-              type="submit"
-              >{{ updateButtonText }}</gov-button
-            >
-            <ck-submit-error v-if="form.$errors.any()" />
+              <gov-button v-if="form.$submitting" disabled type="submit"
+                >Requesting...</gov-button
+              >
+              <gov-button
+                v-else
+                @click="onSubmit"
+                :disabled="loading || !event"
+                type="submit"
+                >{{ updateButtonText }}</gov-button
+              >
+              <ck-submit-error v-if="form.$errors.any()" />
+            </template>
           </gov-grid-column>
         </gov-grid-row>
       </gov-main-wrapper>
@@ -115,14 +130,16 @@ export default {
     return {
       tabs: [
         { id: "details", heading: "Details", active: true },
-        { id: "taxonomies", heading: "Taxonomies", active: false }
+        { id: "taxonomies", heading: "Taxonomies", active: false },
       ],
       organisations: [{ text: "Please select", value: null }],
+      updateRequestCreated: false,
+      updateRequestMessage: null,
       loading: false,
       event: null,
       form: null,
       imageChanged: false,
-      altTextChanged: false
+      altTextChanged: false,
     };
   },
 
@@ -130,7 +147,7 @@ export default {
     allowedTabs() {
       if (!this.auth.isGlobalAdmin) {
         const taxonomiesTabIndex = this.tabs.findIndex(
-          tab => tab.id === "taxonomies"
+          (tab) => tab.id === "taxonomies"
         );
         const tabs = this.tabs.slice();
         tabs.splice(taxonomiesTabIndex, 1);
@@ -142,16 +159,16 @@ export default {
     },
     updateButtonText() {
       return this.auth.isSuperAdmin ? "Duplicate" : "Request duplicate";
-    }
+    },
   },
 
   methods: {
     async fetchOrganisations() {
       this.loading = true;
       let fetchedOrganisations = await this.fetchAll("/organisations", {
-        "filter[has_permission]": true
+        "filter[has_permission]": true,
       });
-      fetchedOrganisations = fetchedOrganisations.map(organisation => {
+      fetchedOrganisations = fetchedOrganisations.map((organisation) => {
         return { text: organisation.name, value: organisation.id };
       });
       this.organisations = [...this.organisations, ...fetchedOrganisations];
@@ -186,12 +203,12 @@ export default {
         booking_url: this.event.booking_url || "",
         booking_cta: this.event.booking_cta || "",
         is_virtual: this.event.is_virtual,
-        homepage: this.event.homepage || false,
+        homepage: false,
         location_id: this.event.location_id,
         image_file_id: null,
         category_taxonomies: this.event.category_taxonomies.map(
-          taxonomy => taxonomy.id
-        )
+          (taxonomy) => taxonomy.id
+        ),
       });
 
       this.loading = false;
@@ -201,24 +218,26 @@ export default {
       if (!this.event) {
         return;
       }
-      if (this.imageChanged && (!this.altTextChanged && (!this.event.image || !this.event.image.alt_text))) {
-        this.form.$errors.record({"alt_text": ["Please enter alt text for the image."]});
+      if (this.imageChanged && !this.altTextChanged) {
+        this.form.$errors.record({
+          alt_text: ["Please enter alt text for the image."],
+        });
         return;
       }
       if (this.imageChanged) {
-        this.form.$errors.record({"file": ["Please click 'Upload file' to upload your image."]});
+        this.form.$errors.record({
+          file: ["Please click 'Upload file' to upload your image."],
+        });
         return;
       }
-      const response = await this.form.post(
-        `/organisation-events`
-      );
+      const response = await this.form.post(`/organisation-events`);
 
       const eventId = response.data.id;
 
       if (this.auth.isSuperAdmin && eventId) {
         this.$router.push({
           name: "events-show",
-          params: { event: eventId }
+          params: { event: eventId },
         });
       } else if (!this.form.$errors.any()) {
         this.updateRequestCreated = true;
@@ -226,25 +245,25 @@ export default {
       }
     },
     onTabChange({ index }) {
-      this.tabs.forEach(tab => (tab.active = false));
+      this.tabs.forEach((tab) => (tab.active = false));
       const tabId = this.allowedTabs[index].id;
-      this.tabs.find(tab => tab.id === tabId).active = true;
+      this.tabs.find((tab) => tab.id === tabId).active = true;
     },
     isTabActive(id) {
-      const tab = this.allowedTabs.find(tab => tab.id === id);
+      const tab = this.allowedTabs.find((tab) => tab.id === id);
 
       return tab === undefined ? false : tab.active;
     },
     onUpdateTitle(title) {
       this.form.title = title;
       this.form.slug = this.slugify(title);
-    }
+    },
   },
 
   created() {
     this.fetchEvent();
     this.fetchOrganisations();
-  }
+  },
 };
 </script>
 
