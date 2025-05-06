6a052e24a42c70509f8c2629adb070b7db3d66aa
byt3sage <jtooleuk@gmail.com>
2025-05-06 16:15:50 +0100

    ACD-460

--- src/views/services/Create.vue
+++ src/views/services/Create.vue
@@ -207,15 +207,16 @@
                 :referral_email.sync="form.referral_email"
                 :referral_url.sync="form.referral_url"
               >
-                <gov-button v-if="form.$submitting" disabled type="submit"
+                
+              </referral-tab>
+            </gov-tabs>
+            <gov-button v-if="form.$submitting" disabled type="submit"
                   >Creating...</gov-button
                 >
                 <gov-button v-else @click="onSubmit" type="submit"
                   >Create</gov-button
                 >
                 <ck-submit-error v-if="form.$errors.any()" />
-              </referral-tab>
-            </gov-tabs>
           </template>
         </gov-grid-column>
       </gov-grid-row>
