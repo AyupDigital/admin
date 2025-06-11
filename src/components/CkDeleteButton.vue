b2d722b95d6a41ca791e3c30d2041ca21d3c1c19
byt3sage <jtooleuk@gmail.com>
2025-06-11 15:33:14 +0100

    ACD-497

--- src/components/CkDeleteButton.vue
+++ src/components/CkDeleteButton.vue
@@ -8,14 +8,12 @@
 
     <!-- If the user has clicked the delete button, they must then confirm -->
     <template v-else>
-      <gov-error-summary title="This action will be logged">
+      <gov-error-summary title="Confirm deletion">
         <gov-body>
-          Deleting this {{ resource }} will also delete all associated data. Are
-          you sure you want to delete this {{ resource }}? This action is
-          irreversible.
+          Are you sure you want to delete this {{ resource }}? This action is immediate and can not be undone.
         </gov-body>
 
-        <gov-button @click="onDelete" error>Confirm deletion</gov-button
+        <gov-button @click="onDelete" error>Delete</gov-button
         >&nbsp;<!--
      --><gov-button @click="onCancel">Cancel</gov-button>
       </gov-error-summary>
